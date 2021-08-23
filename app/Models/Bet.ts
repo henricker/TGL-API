da252deb53d9a2e3155e3f7eb9621a141dbd7933
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 13:39:20 -0300

    feat: create Bet model and add relationship with users and games

---
+++ app/Models/Bet.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+import Game from './Game'
+
+export default class Bet extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public numbers: string
+
+  @column()
+  public userId: number
+
+  @column()
+  public gameId: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @belongsTo(() => Game)
+  public game: BelongsTo<typeof Game>
+}
