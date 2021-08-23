a3541f9a44eaed497a837a79e5f5e2230d00816f
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 09:08:51 -0300

    feat: add session validator

---
+++ app/Validators/SessionValidator.ts
@@ -0,0 +1,16 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class SessionValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    email: schema.string({}, [rules.email()]),
+    password: schema.string({}),
+  })
+
+  public messages = {
+    'required': '{{ field }} is required',
+    'email.email': 'email invalid',
+  }
+}
