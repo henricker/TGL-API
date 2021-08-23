4508fb84c72e847239bb482864f959e39d4192af
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 13:35:35 -0300

    feat: create games migration

---
+++ database/migrations/1629736137649_bets.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Bets extends BaseSchema {
+  protected tableName = 'bets'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.text('numbers').notNullable()
+      table.integer('user_id').unsigned().references('id').inTable('users').notNullable()
+      table.integer('game_id').unsigned().references('id').inTable('games').notNullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
