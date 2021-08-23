a2d3758f904eae6c647e37373de6b200d801801f
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 09:26:49 -0300

    feat: create AuthAdmin middleware

---
+++ app/Middleware/AuthAdmin.ts
@@ -0,0 +1,12 @@
+import { AuthenticationException } from '@adonisjs/auth/build/standalone'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthAdmin {
+  public async handle({ auth }: HttpContextContract, next: () => Promise<void>) {
+    const { isAdmin } = await auth.use('api').authenticate()
+
+    if (!isAdmin) throw new AuthenticationException('You are not authorized for this route.', '401')
+
+    await next()
+  }
+}
