8acdbde196e034c46fc9cbba23a5477c987c6f87
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 10:21:35 -0300

    feat: add admin routes

--- start/routes.ts
+++ start/routes.ts
@@ -3,6 +3,11 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.post('/session', 'SessionsController.store')
 
 Route.group(() => {
+  Route.post('/', 'ForgotPasswordsController.store')
+  Route.put('/recovery', 'ForgotPasswordsController.update')
+}).prefix('/forgot-password')
+
+Route.group(() => {
   Route.post('/create', 'UsersController.create')
   Route.group(() => {
     Route.get('/index', 'UsersController.index')
@@ -12,6 +17,11 @@ Route.group(() => {
 }).prefix('/users')
 
 Route.group(() => {
-  Route.post('/', 'ForgotPasswordsController.store')
-  Route.put('/recovery', 'ForgotPasswordsController.update')
-}).prefix('/forgot-password')
+  Route.patch('/promote-user/:userId', 'AdminsController.promoteUser')
+  Route.patch('/demote-user/:userId', 'AdminsController.demoteUser')
+  Route.delete('/delete-user/:userId', 'AdminsController.delete')
+  Route.get('/all-users', 'AdminsController.getAllUsers')
+})
+  .prefix('/admin')
+  .middleware('auth')
+  .middleware('authAdmin')
