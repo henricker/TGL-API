d2f2c5b22a36844b5faa948c39b0eac8a960059e
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 11:56:48 -0300

    feat: create game migration

--- database/migrations/1629725480726_games.ts
+++ database/migrations/1629725480726_games.ts
@@ -6,10 +6,10 @@ export default class Games extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('game_type').notNullable()
+      table.string('type').notNullable()
       table.text('description').notNullable()
       table.integer('range').notNullable()
-      table.float('price').notNullable()
+      table.decimal('price').notNullable()
       table.integer('max_number').notNullable()
       table.string('color').notNullable()
       table.integer('min_cart_value').notNullable()
