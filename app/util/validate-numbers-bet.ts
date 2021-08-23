d77d8c33cc05bb07b98a011969df47568cba35d5
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 17:05:06 -0300

    feat: create util function to check values of bet

---
+++ app/util/validate-numbers-bet.ts
@@ -0,0 +1,23 @@
+import Game from 'App/Models/Game'
+
+export default function validateBetNumbers(numbers: number[], game: Game): Object | undefined {
+  if (numbers.length !== game.maxNumber) {
+    return {
+      errors: [{ message: `The ${game.type} only allows ${game.maxNumber} numbers choosen` }],
+    }
+  }
+
+  numbers.forEach((value) => {
+    if (value > game.range) {
+      return {
+        errors: [
+          {
+            message: `The ${game.type} only accepts values less than or equal to ${game.range}`,
+          },
+        ],
+      }
+    }
+  })
+
+  return undefined
+}
