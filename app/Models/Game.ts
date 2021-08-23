aafa983a8225886a71265442c4cf09c57fbb72b6
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 11:57:08 -0300

    feat: create game model

---
+++ app/Models/Game.ts
@@ -0,0 +1,34 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Game extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public type: string
+
+  @column()
+  public description: string
+
+  @column()
+  public range: number
+
+  @column()
+  public price: number
+
+  @column()
+  public color: string
+
+  @column()
+  public maxNumber: number
+
+  @column()
+  public minCartValue: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
