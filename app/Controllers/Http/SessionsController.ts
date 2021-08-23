84a3079bc51f94fa5a7282d33879802e6a55b048
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 09:09:31 -0300

    feat: add session controller

---
+++ app/Controllers/Http/SessionsController.ts
@@ -0,0 +1,13 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import SessionValidator from 'App/Validators/SessionValidator'
+
+export default class SessionsController {
+  public async store({ request, auth }: HttpContextContract) {
+    const data = await request.validate(SessionValidator)
+    const token = await auth.use('api').attempt(data.email, data.password, {
+      expiresIn: '1days',
+    })
+
+    return { token }
+  }
+}
