a8b5c435abbe93da57f43f5aa4d0747902fcb37d
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 20:30:43 -0300

    feat: add new-bet mailer class

---
+++ app/Mailers/NewBet.ts
@@ -0,0 +1,18 @@
+import { BaseMailer, MessageContract } from '@ioc:Adonis/Addons/Mail'
+import User from 'App/Models/User'
+
+export default class NewBet extends BaseMailer {
+  constructor(private user: User, private totalPrice: string) {
+    super()
+  }
+  public prepare(message: MessageContract) {
+    message
+      .subject('New bet')
+      .from('https://tgl.luby@suport.com', 'TGL Bets')
+      .to(this.user.email)
+      .htmlView('mails/new-bet', {
+        name: this.user.name,
+        totalPrice: this.totalPrice,
+      })
+  }
+}
