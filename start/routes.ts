88f0c19460bddcfe6a6f7038d4fc46f6a2e9cd92
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 11:55:17 -0300

    feat: add game routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,6 +1,8 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/session', 'SessionsController.store')
+Route.get('/games', 'GamesController.index')
+Route.get('/games/:id', 'GamesController.show')
 
 Route.group(() => {
   Route.post('/', 'ForgotPasswordsController.store')
@@ -17,10 +19,18 @@ Route.group(() => {
 }).prefix('/users')
 
 Route.group(() => {
-  Route.patch('/promote-user/:userId', 'AdminsController.promoteUser')
-  Route.patch('/demote-user/:userId', 'AdminsController.demoteUser')
-  Route.delete('/delete-user/:userId', 'AdminsController.delete')
-  Route.get('/all-users', 'AdminsController.getAllUsers')
+  Route.group(() => {
+    Route.patch('/promote-user/:userId', 'AdminsController.promoteUser')
+    Route.patch('/demote-user/:userId', 'AdminsController.demoteUser')
+    Route.delete('/delete-user/:userId', 'AdminsController.deleteUser')
+    Route.get('/all-users', 'AdminsController.getAllUsers')
+  }).prefix('/users')
+
+  Route.group(() => {
+    Route.post('/create-game', 'GamesController.store')
+    Route.put('/update-game/:id', 'GamesController.update')
+    Route.delete('/delete-game/:id', 'GamesController.destroy')
+  }).prefix('/games')
 })
   .prefix('/admin')
   .middleware('auth')
