5ef3a5636b06455ed1eb519f9d056b33eb1d0b0f
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 09:09:22 -0300

    feat: add forgot-password controller

---
+++ app/Controllers/Http/ForgotPasswordsController.ts
@@ -0,0 +1,35 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import RecoveryPasswordValidator from 'App/Validators/forgot-password-validator/RecoveryPasswordValidator'
+import ResetPasswordValidator from 'App/Validators/forgot-password-validator/ResetPasswordValidator'
+import crypto from 'crypto'
+import { DateTime } from 'luxon'
+import moment from 'moment'
+import { ValidationException } from '@ioc:Adonis/Core/Validator'
+
+export default class ForgotPasswordsController {
+  public async store({ request }: HttpContextContract) {
+    const data = await request.validate(ResetPasswordValidator)
+    const user = await User.findByOrFail('email', data.email)
+
+    user.rememberMeToken = crypto.randomBytes(12).toString('hex')
+    user.tokenCreatedAt = DateTime.now()
+
+    await user.save()
+  }
+
+  public async update({ request }: HttpContextContract) {
+    const { password, token } = await request.validate(RecoveryPasswordValidator)
+    const user = await User.findByOrFail('remember_me_token', token)
+
+    const tokenExpired = moment().subtract('2', 'days').isAfter(user.tokenCreatedAt)
+
+    if (tokenExpired) throw new ValidationException(false, 'Token expired')
+
+    user.rememberMeToken = null
+    user.tokenCreatedAt = null
+    user.password = password
+
+    await user.save()
+  }
+}
