2e7af77c4c0b3caca2666e45b219bdf95603579f
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-23 21:36:44 -0300

    feat: shedule mails to users when not bet in more than seven days

---
+++ app/Tasks/RememberUserToBer.ts
@@ -0,0 +1,23 @@
+import { BaseTask } from 'adonis5-scheduler/build'
+import RememberBet from 'App/Mailers/RememberBet'
+import User from 'App/Models/User'
+import moment from 'moment'
+
+export default class RememberUserToBet extends BaseTask {
+  public static get schedule() {
+    return '* * * * * *'
+  }
+
+  public static get useLock() {
+    return false
+  }
+
+  public async handle() {
+    const users = await User.query()
+    users.forEach(async (user) => {
+      if (user.lastBet && moment().subtract('7', 'days').isAfter(user.lastBet) && !user.isAdmin) {
+        await new RememberBet(user).sendLater()
+      }
+    })
+  }
+}
