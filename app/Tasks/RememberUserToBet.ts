3e6306fc96fca8cce98e52b3d099636cd7df76df
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-24 16:36:45 -0300

    style: rename archive

---
+++ app/Tasks/RememberUserToBet.ts
@@ -0,0 +1,22 @@
+import { BaseTask } from 'adonis5-scheduler/build'
+import RememberBet from 'App/Mailers/RememberBet'
+import User from 'App/Models/User'
+import moment from 'moment'
+
+export default class RememberUserToBet extends BaseTask {
+  public static get schedule() {
+    return '0 0 9 * * *'
+  }
+
+  public static get useLock() {
+    return true
+  }
+
+  public async handle() {
+    const users = await User.query()
+    users.forEach(async (user) => {
+      if (user.lastBet && moment().subtract('7', 'days').isAfter(user.lastBet) && !user.isAdmin)
+        await new RememberBet(user).sendLater()
+    })
+  }
+}
