5db18d491ecd9fcae1ae26b69c8bb4c96a6478b7
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-25 10:52:19 -0300

    style: add prettier

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -7,7 +7,6 @@ import moment from 'moment'
 export default class UsersController {
   public async create({ request, auth }: HttpContextContract) {
     const data = await request.validate(CreateUserValidator)
-
     const user = await User.create(data)
     const token = await auth.use('api').attempt(data.email, data.password)
     return { user, token }
