bbdcf104a43eafeb670c6adfad5265fa8952f522
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-08-25 10:52:39 -0300

    refactor: add formatter function to evict duplacation code

--- app/Controllers/Http/GamesController.ts
+++ app/Controllers/Http/GamesController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Game from 'App/Models/Game'
+import { formatter } from 'App/util/formatter-real'
 import GameValidator from 'App/Validators/GameValidator'
 import moment from 'moment'
 
@@ -10,10 +11,7 @@ export default class GamesController {
       let gamesJSON = (await Game.query().paginate(page ?? 1, limit ?? 15)).serialize()
 
       gamesJSON.data = gamesJSON.data.map((game) => {
-        game.price = game.price.toLocaleString('pt-BR', {
-          style: 'currency',
-          currency: 'BRL',
-        })
+        game.price = formatter.format(game.price)
         game.created_at = moment(game.created_at).format('DD/MM/YYYY HH:MM:SS')
         game.updated_at = moment(game.updated_at).format('DD/MM/YYYY HH:MM:SS')
         return game
@@ -29,7 +27,7 @@ export default class GamesController {
     const data = await request.validate(GameValidator)
     let game = (await Game.create(data)).serialize()
 
-    game.price = game.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    game.price = formatter.format(game.price)
 
     return game
   }
@@ -37,10 +35,7 @@ export default class GamesController {
   public async show({ params, response }: HttpContextContract) {
     try {
       let gameJSON = (await Game.findByOrFail('id', params.id)).serialize()
-      gameJSON.price = gameJSON.price.toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-      })
+      gameJSON.price = formatter.format(gameJSON.price)
       gameJSON.created_at = moment(gameJSON.createdAt).format('DD/MM/YYYY HH:MM:SS')
       gameJSON.updated_at = moment(gameJSON.updatedAt).format('DD/MM/YYYY HH:MM:SS')
       return gameJSON
