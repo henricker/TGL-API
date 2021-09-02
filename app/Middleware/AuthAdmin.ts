12c7bcd43edf35c53743cb6383eed4b724f4cac4
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-02 12:06:19 -0300

    fix: change code to 'E_UNAUTHORIZED_ACCESS'

--- app/Middleware/AuthAdmin.ts
+++ app/Middleware/AuthAdmin.ts
@@ -5,7 +5,11 @@ export default class AuthAdmin {
   public async handle({ auth }: HttpContextContract, next: () => Promise<void>) {
     const { isAdmin } = await auth.use('api').authenticate()
 
-    if (!isAdmin) throw new AuthenticationException('You are not authorized for this route.', '401')
+    if (!isAdmin)
+      throw new AuthenticationException(
+        'You are not authorized for this route.',
+        'E_UNAUTHORIZED_ACCESS'
+      )
 
     await next()
   }
