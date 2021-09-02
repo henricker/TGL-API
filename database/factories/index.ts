862f4297615726fba5ccfd8e8d20d576047889ae
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-02 14:51:43 -0300

    refactor: add GameFactory

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,4 +1,5 @@
 import Factory from '@ioc:Adonis/Lucid/Factory'
+import Game from 'App/Models/Game'
 import User from 'App/Models/User'
 
 export const UserFactory = Factory.define(User, ({ faker }) => {
@@ -7,5 +8,17 @@ export const UserFactory = Factory.define(User, ({ faker }) => {
     email: faker.internet.email(),
     password: faker.internet.password(),
     isAdmin: false,
+  }
+}).build()
+
+export const GameFactory = Factory.define(Game, ({ faker }) => {
+  return {
+    type: faker.random.word(),
+    description: faker.lorem.paragraph(),
+    range: faker.datatype.number(),
+    price: faker.datatype.number(),
+    maxNumber: faker.datatype.number(),
+    color: faker.internet.color(),
+    minCartValue: faker.datatype.number(),
   }
 }).build()
