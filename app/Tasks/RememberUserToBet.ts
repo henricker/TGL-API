9e817078ea2e5011d6d50948db59555684c3647c
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-13 21:23:50 -0300

    refactor: add mailer producer on sheduler

--- app/Tasks/RememberUserToBet.ts
+++ app/Tasks/RememberUserToBet.ts
@@ -1,6 +1,7 @@
 import { BaseTask } from 'adonis5-scheduler/build'
-import RememberBet from 'App/Mailers/RememberBet'
+//import RememberBet from 'App/Mailers/RememberBet'
 import User from 'App/Models/User'
+import producer from 'kafka-producer/producer'
 import moment from 'moment'
 
 export default class RememberUserToBet extends BaseTask {
@@ -15,8 +16,25 @@ export default class RememberUserToBet extends BaseTask {
   public async handle() {
     const users = await User.query()
     users.forEach(async (user) => {
-      if (user.lastBet && moment().subtract('7', 'days').isAfter(user.lastBet) && !user.isAdmin)
-        await new RememberBet(user).sendLater()
+      if (user.lastBet && moment().subtract('7', 'days').isAfter(user.lastBet) && !user.isAdmin) {
+        //await new RememberBet(user).sendLater()
+        await producer.connect()
+        await producer.sendMessage(
+          [
+            {
+              value: JSON.stringify({
+                contact: {
+                  name: user.name,
+                  email: user.email,
+                },
+                template: 'remember-user-to-bet',
+              }),
+            },
+          ],
+          'mailer-event'
+        )
+        await producer.disconect()
+      }
     })
   }
 }
