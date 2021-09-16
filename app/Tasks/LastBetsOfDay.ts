522a533b650d381282f17fc270c2bc284943ac34
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-16 10:02:10 -0300

    refactor: change schedule to 0 0 17 * * *

--- app/Tasks/LastBetsOfDay.ts
+++ app/Tasks/LastBetsOfDay.ts
@@ -4,10 +4,11 @@ import User from 'App/Models/User'
 import producer from '../../kafka-producer/producer'
 import moment from 'moment'
 import Game from '../Models/Game'
+import { formatter } from 'App/util/formatter-real'
 
 export default class LastBetsOfDayTask extends BaseTask {
   public static get schedule() {
-    return '*/30 * * * * *'
+    return '0 0 17 * * *'
   }
 
   public static get useLock() {
@@ -15,13 +16,14 @@ export default class LastBetsOfDayTask extends BaseTask {
   }
 
   public async handle() {
-    const admin = await User.query().where('isAdmin', true) //get all admins
+    const admin = await User.query().where('isAdmin', true)
     const games = await Game.query()
     const bets = (await Bet.query().preload('game')).filter((bet) =>
-      moment().subtract('0', 'days').isAfter(bet.createdAt)
+      moment().subtract('1', 'day').isBefore(bet.createdAt.toJSDate())
     )
 
     const gamesPlaced = bets.map((bet) => bet.game)
+    let totalPrice = 0
     const gamesRelatory = games.map((game) => {
       let amountOfBets = 0
       let totalPriceOfGame = 0
@@ -29,16 +31,15 @@ export default class LastBetsOfDayTask extends BaseTask {
       const gameSelected = gamesPlaced.filter((gamePlaced) => gamePlaced.type === game.type)
       totalPriceOfGame = gameSelected.length * game.price
       amountOfBets = gameSelected.length
+      totalPrice += totalPriceOfGame
 
       return {
         game: game.type,
         color: game.color,
         amountOfBets,
-        totalPriceOfGame,
+        totalPriceOfGame: formatter.format(totalPriceOfGame),
       }
     })
-    let totalPrice = 0
-    gamesRelatory.forEach((game) => (totalPrice += game.totalPriceOfGame))
 
     admin.forEach(async (adm) => {
       await producer.connect()
@@ -51,7 +52,7 @@ export default class LastBetsOfDayTask extends BaseTask {
                 email: adm.email,
               },
               games: {
-                totalPrice,
+                totalPrice: formatter.format(totalPrice),
                 gamesRelatory,
               },
               template: 'new-bets-admin',
@@ -64,10 +65,3 @@ export default class LastBetsOfDayTask extends BaseTask {
     })
   }
 }
-
-/*
-{
-  "type": 'Mega-Sena",
-  "color": ''
-}
-*/
